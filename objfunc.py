96e51383632f84961b78d50d04472a2cf06fa24d
emailisgone <nikita.exe0@gmail.com>
2024-10-15 13:14:55 +0300

    Minor fixes

--- objfunc.py
+++ objfunc.py
@@ -6,6 +6,4 @@ def f(x):
 
 def gradF(x):
     x1, x2 = x
-    dfdx1 = -1/8*(x2-2*x1*x2-x2**2)
-    dfdx2 = -1/8*(x1-2*x1*x2-x1**2)
-    return np.array([dfdx1, dfdx2])
+    return [-1/8*(x2-2*x1*x2-x2**2), -1/8*(x1-2*x1*x2-x1**2)]
